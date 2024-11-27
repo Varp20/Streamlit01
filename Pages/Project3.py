fa6509ed6a3ef5b019cf3469c309905478adb2de
Varp20 <83496662+Varp20@users.noreply.github.com>
2024-11-27 23:59:14 +0500

    0

--- Pages/Project3.py
+++ Pages/Project3.py
@@ -3,53 +3,51 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def load_data(file):
-    if file is not None:
-        data = pd.read_csv(file)
-        return data
-    return None
-
-
-def clean_and_convert_values(df, column):
-    df[column] = df[column].astype(str)
-
-    df[column] = df[column].replace({',':'.'},regex=True)
-    df[column] = df[column].str.replace(r'\s+','', regex=True)
-
-    df[column] = pd.to_numeric(df[column], errors='coerce')
-
-    return df
-
-
-def plot_bar_chart(df, category_column, value_column):
-    df= clean_and_convert_values(df, value_column)
-
-    if df[category_column].dtype == 'object' or df[category_column].dtype.name== 'category':
-        if df[value_column].dtype in ['int64', 'float64']:
-
-            category_mean =df.groupby(category_column)[value_column].mean()
-
-            colors= plt.cm.get_cmap('tab20', len(category_mean))
-
-            fig, ax = plt.subplots()
-            category_mean.plot ( kind='bar', ax=ax, color=colors(np.arange(len(category_mean))))
-            ax.set_title(f'Mean {value_column} by {category_column}')
-            ax.set_xlabel(category_column)
-            ax.set_ylabel(f'Mean {value_column} ')
-            st.pyplot(fig)
-
-            st.write(f"Mean {value_column} by {category_column}:")
-            st.dataframe(category_mean)
-        else:
-            st.warning(f"{value_column} is not a numeric column. Please select a numeric column to get values")
-    else:
-        st.warning(f"{category_column} is not a categorical column. Please select a categorical column to comply")
-
-
 class Project3:
-    def __int__(self):
+    def __init__(self):
         pass
+    def load_data(self, file):
+        if file is not None:
+            data = pd.read_csv(file)
+            return data
+        return None
+
+
+    def clean_and_convert_values(self, df, column):
+        df[column] = df[column].astype(str)
+
+        df[column] = df[column].replace({',':'.'},regex=True)
+        df[column] = df[column].str.replace(r'\s+','', regex=True)
+
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+
+        return df
+
+
+    def plot_bar_chart(self, df, category_column, value_column):
+        df= self.clean_and_convert_values(df, value_column)
+
+        if df[category_column].dtype == 'object' or df[category_column].dtype.name== 'category':
+            if df[value_column].dtype in ['int64', 'float64']:
+
+                category_mean =df.groupby(category_column)[value_column].mean()
+
+                colors= plt.cm.get_cmap('tab20', len(category_mean))
+
+                fig, ax = plt.subplots()
+                category_mean.plot ( kind='bar', ax=ax, color=colors(np.arange(len(category_mean))))
+                ax.set_title(f'Mean {value_column} by {category_column}')
+                ax.set_xlabel(category_column)
+                ax.set_ylabel(f'Mean {value_column} ')
+                st.pyplot(fig)
+
+                st.write(f"Mean {value_column} by {category_column}:")
+                st.dataframe(category_mean)
+            else:
+                st.warning(f"{value_column} is not a numeric column. Please select a numeric column to get values")
+        else:
+            st.warning(f"{category_column} is not a categorical column. Please select a categorical column to comply")
+
 
     def app(self):
         st.title("Creation of DataFrame")
@@ -74,6 +72,7 @@ class Project3:
                     text-align: center;
                     }
                     </style>""", unsafe_allow_html=True)
+
 if __name__ == '__main__':
     project=Project3()
     project.app()
